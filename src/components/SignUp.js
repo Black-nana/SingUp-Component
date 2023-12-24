fbca31125928c6ebbbbecad14e6008eb24dafb2d
Abdul Mateen Kofi Yeboah <126984060+Black-nana@users.noreply.github.com>
2023-12-24 00:22:43 +0000

    update set

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -8,27 +8,40 @@ import img2 from '../asserts/jocke-wulcan-KLOW1bD616Y-unsplash.jpg';
 
 const validationSchema = yup.object().shape({
   fullName: yup.string().required('Full Name is required'),
-  email: yup.string().email('Invalid email address').required('Email is required'),
-  password: yup.string().min(8, 'Must be 8 characters or more').required('Password is required'),
+  email: yup
+    .string()
+    .email('Invalid email address')
+    .required('Email is required'),
+  password: yup
+    .string()
+    .min(8, 'Must be 8 characters or more')
+    .required('Password is required'),
 });
 
 const SignUp = () => {
   const {
     register,
     handleSubmit,
+    reset: Reset,
     formState: { errors },
   } = useForm({
     resolver: yupResolver(validationSchema),
   });
 
-  const onSubmit = (data) => console.log(data);
-
+  const onSubmit = (data) => {
+    console.log(data);
+    Reset();
+  };
   return (
     <div>
       <div className='grid grid-cols-2 max-md:grid'>
         <div className='h-full relative max-md:hidden'>
           <div className='absolute inset-0 bg-black opacity-50'></div>
-          <img src={img2} alt='' className='h-screen w-full object-cover' />
+          <img
+            src={img2}
+            alt=''
+            className='h-screen w-full object-cover'
+          />
           <p className='absolute top-1/3 p-20 text-white text-4xl'>
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quisquam a
           </p>
@@ -37,11 +50,15 @@ const SignUp = () => {
         <div className='grid place-items-center w-full max-md:absolute h-screen'>
           <div className='form-container w-3/4 grid gap-4 h-3/4'>
             <h1 className='font-bold text-3xl flex justify-items-center justify-center gap-2 max-md:h-14'>
-              <span className='border-b-4 border-green-600'>Create</span> Account
+              <span className='border-b-4 border-green-600'>Create</span>{' '}
+              Account
               <RiAccountBoxFill className='text-green-600 text-4xl group' />
             </h1>
             <form onSubmit={handleSubmit(onSubmit)}>
-              <div className={`input-group relative ${errors.fullName ? 'border-red-500' : ''}`}>
+              <div
+                className={`input-group relative ${
+                  errors.fullName ? 'border-red-500' : ''
+                }`}>
                 <FaUser className='absolute top-4 ml-2 w-6 text-green-600' />
                 <div className='w-full'>
                   <input
@@ -51,11 +68,16 @@ const SignUp = () => {
                     {...register('fullName')}
                   />
                   {errors.fullName && (
-                    <p className='text-sm text-red-500 m-0 p-0'>{errors.fullName.message}</p>
+                    <p className='text-sm text-red-500 m-0 p-0'>
+                      {errors.fullName.message}
+                    </p>
                   )}
                 </div>
               </div>
-              <div className={`input-group relative ${errors.email ? 'border-red-500' : ''}`}>
+              <div
+                className={`input-group relative ${
+                  errors.email ? 'border-red-500' : ''
+                }`}>
                 <FaEnvelope className='absolute top-4 ml-2 w-6 text-green-600' />
                 <div className='w-full'>
                   <input
@@ -65,11 +87,16 @@ const SignUp = () => {
                     {...register('email')}
                   />
                   {errors.email && (
-                    <p className='text-sm text-red-500 m-0 p-0'>{errors.email.message}</p>
+                    <p className='text-sm text-red-500 m-0 p-0'>
+                      {errors.email.message}
+                    </p>
                   )}
                 </div>
               </div>
-              <div className={`input-group relative ${errors.password ? 'border-red-500' : ''}`}>
+              <div
+                className={`input-group relative ${
+                  errors.password ? 'border-red-500' : ''
+                }`}>
                 <FaLock className='absolute top-4 ml-2 w-6 text-green-600' />
                 <div className='w-full'>
                   <input
@@ -79,7 +106,9 @@ const SignUp = () => {
                     {...register('password')}
                   />
                   {errors.password && (
-                    <p className='text-sm text-red-500 m-0 p-0'>{errors.password.message}</p>
+                    <p className='text-sm text-red-500 m-0 p-0'>
+                      {errors.password.message}
+                    </p>
                   )}
                 </div>
               </div>
